4ece760bb5b332fa5c2362987be7ce05bb1f1b71
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-10-26 16:53:12 +0800

    Put contents of ToDoTile.js

--- src/components/ToDoTile.js
+++ src/components/ToDoTile.js
@@ -1,5 +1,24 @@
 import { useState } from "react"
 
 export default function ToDoTile( { props, deleteToDo } ) {
-    return <div>To-Do Tile</div>
+    const [checked, setChecked] = useState(false);
+
+    function handleChange(e) {
+        setChecked(!checked);
+    }
+
+    return <div className="ToDo-Tile">
+        <label className="ToDo-Details">
+            <input 
+                type="checkbox" 
+                id={ props.id } 
+                key={ props.id }
+                checked={ checked }
+                onChange={ handleChange }></input> 
+            <span>
+                { checked ? <strike> { props.name } </strike> : props.name }
+            </span>
+        </label>
+        <button className="ToDo-Delete" onClick={() => deleteToDo(props.id)}>×</button>
+    </div>
 }
