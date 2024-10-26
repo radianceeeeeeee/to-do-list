fdad7025a63f610480611aea4004329544f9c055
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-10-26 16:50:33 +0800

    Added template contents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ToDoTile from './components/ToDoTile';
+import Image from './assets/totodile.png';
+
+var currentTaskID = 0;
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [newTaskName, setNewTaskName] = useState("");
+
+  function deleteToDo(id) {
+    const remainingTasks = toDoList.filter((task) => task.id !== id);
+    setToDoList(remainingTasks);
+  }
+
+  function handleTaskName(e) {
+    setNewTaskName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newTaskName === "") return;
+
+    const newToDo = {id: currentTaskID, name: newTaskName};
+    setToDoList([...toDoList, newToDo]);
+    setNewTaskName("");
+    currentTaskID++;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <form className="App-Header" onSubmit={handleSubmit}>
+        <div className="App-Title">To-Do List</div>
+          <input 
+            placeholder='To-Do Tile' 
+            value={newTaskName}
+            onChange={handleTaskName}
+            className="App-TaskName"
+          ></input>
+          <button type="submit" className="App-AddTask"> + Add Task</button>
+        </form>
       </header>
+
+      <div className="App-Body">
+        {toDoList.length === 0 ? 
+          <div className='No-Tasks'>
+            Add tasks first
+          </div> :
+          toDoList.map((toDo) => (
+            <ToDoTile props={toDo} deleteToDo={() => deleteToDo(toDo.id)}/>
+          ))
+        }
+      </div>
+
+      <footer className="App-Footer">
+        Totodile Wallpaper from <a href='https://www.deviantart.com/dannymybrother/art/Totodile-597327401'>DannyMyBrother</a>
+      </footer>
+    <div className="App-Background"><img src={Image} height={200}></img></div>
     </div>
   );
 }
---
+++ src/components/ToDoTile.js
@@ -0,0 +1,5 @@
+import { useState } from "react"
+
+export default function ToDoTile( { props, deleteToDo } ) {
+    return <div>To-Do Tile</div>
+}
